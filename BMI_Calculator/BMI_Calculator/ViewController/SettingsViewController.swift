9ad057b721782a8ab91db04a19fc86be4a9b4bef
Roja <roja@3frameslab.com>
2022-12-14 21:28:02 +0530

    update customcells

--- BMI_Calculator/BMI_Calculator/ViewController/SettingsViewController.swift
+++ BMI_Calculator/BMI_Calculator/ViewController/SettingsViewController.swift
@@ -20,6 +20,8 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Settings"
+        
         //ConfigerCustomCells Here.
         setCustomCells()
         
